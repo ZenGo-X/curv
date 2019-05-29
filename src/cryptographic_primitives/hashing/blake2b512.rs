f3e37a0d6a3953cacf95e00b688237e66c94658f
omershlo <omer.shlomovits@gmail.com>
2019-05-29 20:55:16 +0300

    hash from ge

--- src/cryptographic_primitives/hashing/blake2b512.rs
+++ src/cryptographic_primitives/hashing/blake2b512.rs
@@ -7,7 +7,8 @@
 
 use arithmetic::traits::Converter;
 use blake2_rfc::blake2b::Blake2b;
-use BigInt;
+use elliptic::curves::traits::{ECPoint, ECScalar};
+use {BigInt, FE, GE};
 
 pub struct Blake;
 
@@ -19,6 +20,17 @@ impl Blake {
         }
 
         BigInt::from(digest.finalize().as_ref())
+    }
+
+    fn create_hash_from_ge(ge_vec: &[&GE], persona: &[u8]) -> FE {
+        let mut digest = Blake2b::with_params(64, &[], &[], persona);
+
+        for value in ge_vec {
+            digest.update(&value.pk_to_key_slice());
+        }
+
+        let result = BigInt::from(digest.finalize().as_ref());
+        ECScalar::from(&result)
     }
 }
 
@@ -36,6 +48,20 @@ mod tests {
         let result =
             Blake::create_hash(&vec![&BigInt::one(), &BigInt::zero()], b"Zcash_RedJubjubH");
         assert!(result > BigInt::zero());
+    }
+
+    #[test]
+    fn create_hash_from_ge_test() {
+        let point = GE::base_point2();
+        let result1 =
+            Blake::create_hash_from_ge(&vec![&point, &GE::generator()], b"Zcash_RedJubjubH");
+        assert!(result1.to_big_int().to_str_radix(2).len() > 240);
+        let result2 =
+            Blake::create_hash_from_ge(&vec![&GE::generator(), &point], b"Zcash_RedJubjubH");
+        assert_ne!(result1, result2);
+        let result3 =
+            Blake::create_hash_from_ge(&vec![&GE::generator(), &point], b"Zcash_RedJubjubH");
+        assert_eq!(result2, result3);
     }
 
 }
