e1602bb3c4c9c41f4228c67dbb1f0d04014363c5
Denis Varlakov <dsurv@yandex.ru>
2020-10-31 15:35:41 +0700

    Update coin_flip_optimal_rounds

--- src/cryptographic_primitives/twoparty/coin_flip_optimal_rounds.rs
+++ src/cryptographic_primitives/twoparty/coin_flip_optimal_rounds.rs
@@ -5,52 +5,64 @@
     License MIT: <https://github.com/KZen-networks/curv/blob/master/LICENSE>
 */
 
+use std::fmt::Debug;
+
 use crate::cryptographic_primitives::proofs::sigma_valid_pedersen::PedersenProof;
 use crate::cryptographic_primitives::proofs::sigma_valid_pedersen::ProvePederesen;
 use crate::cryptographic_primitives::proofs::sigma_valid_pedersen_blind::PedersenBlindingProof;
 use crate::cryptographic_primitives::proofs::sigma_valid_pedersen_blind::ProvePederesenBlind;
 use crate::elliptic::curves::traits::*;
-use crate::{FE, GE};
+use zeroize::Zeroize;
 
 /// based on How To Simulate It – A Tutorial on the Simulation
 /// Proof Technique. protocol 7.3: Multiple coin tossing. which provide simulatble constant round
 /// coin toss
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
-pub struct Party1FirstMessage {
-    pub proof: PedersenProof,
+pub struct Party1FirstMessage<P>
+{
+    pub proof: P,
 }
 
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
-pub struct Party2FirstMessage {
-    pub seed: FE,
+pub struct Party2FirstMessage<P: ECPoint> {
+    pub seed: P::Scalar,
 }
 #[derive(Clone, PartialEq, Debug, Serialize, Deserialize)]
-pub struct Party1SecondMessage {
-    pub proof: PedersenBlindingProof,
-    pub seed: FE,
+pub struct Party1SecondMessage<P: ECPoint> {
+    pub proof: PedersenBlindingProof<P>,
+    pub seed: P::Scalar,
 }
-impl Party1FirstMessage {
-    pub fn commit() -> (Party1FirstMessage, FE, FE) {
-        let seed: FE = ECScalar::new_random();
-        let blinding: FE = ECScalar::new_random();
+impl<P> Party1FirstMessage<PedersenProof<P>>
+    where P: ECPoint + Clone,
+          P::Scalar: Zeroize,
+{
+    pub fn commit() -> (Party1FirstMessage<PedersenProof<P>>, P::Scalar, P::Scalar) {
+        let seed: P::Scalar = ECScalar::new_random();
+        let blinding: P::Scalar = ECScalar::new_random();
         let proof = PedersenProof::prove(&seed, &blinding);
         (Party1FirstMessage { proof }, seed, blinding)
     }
 }
-impl Party2FirstMessage {
-    pub fn share(proof: &PedersenProof) -> Party2FirstMessage {
+impl<P> Party2FirstMessage<P>
+where P: ECPoint + Clone,
+      P::Scalar: Zeroize + Clone,
+{
+    pub fn share(proof: &PedersenProof<P>) -> Party2FirstMessage<P> {
         PedersenProof::verify(&proof).expect("{(m,r),c} proof failed");
-        let seed: FE = ECScalar::new_random();
+        let seed: P::Scalar = ECScalar::new_random();
         Party2FirstMessage { seed }
     }
 }
-impl Party1SecondMessage {
+impl<P> Party1SecondMessage<P>
+where P: ECPoint + Clone,
+      P::Scalar: Zeroize + Clone,
+{
     pub fn reveal(
-        party2seed: &FE,
-        party1seed: &FE,
-        party1blinding: &FE,
-    ) -> (Party1SecondMessage, FE) {
-        let proof = PedersenBlindingProof::prove(&party1seed, &party1blinding);
+        party2seed: &P::Scalar,
+        party1seed: &P::Scalar,
+        party1blinding: &P::Scalar,
+    ) -> (Party1SecondMessage<P>, P::Scalar) {
+        let proof = PedersenBlindingProof::<P>::prove(&party1seed, &party1blinding);
         let coin_flip_result = &party1seed.to_big_int() ^ &party2seed.to_big_int();
         (
             Party1SecondMessage {
@@ -63,8 +75,11 @@ impl Party1SecondMessage {
 }
 
 // party2 finalize
-pub fn finalize(proof: &PedersenBlindingProof, party2seed: &FE, party1comm: &GE) -> FE {
-    PedersenBlindingProof::verify(&proof).expect("{r,(m,c)} proof failed");
+pub fn finalize<P>(proof: &PedersenBlindingProof<P>, party2seed: &P::Scalar, party1comm: &P) -> P::Scalar
+where P: ECPoint + Clone + Debug,
+      P::Scalar: Zeroize + Clone,
+{
+    PedersenBlindingProof::<P>::verify(&proof).expect("{r,(m,c)} proof failed");
     assert_eq!(&proof.com, party1comm);
     let coin_flip_result = &proof.m.to_big_int() ^ &party2seed.to_big_int();
     ECScalar::from(&coin_flip_result)
@@ -72,13 +87,17 @@ pub fn finalize(proof: &PedersenBlindingProof, party2seed: &FE, party1comm: &GE)
 
 #[cfg(test)]
 mod tests {
-    use crate::cryptographic_primitives::twoparty::coin_flip_optimal_rounds::*;
-    #[test]
-    pub fn test_coin_toss() {
-        let (party1_first_message, m1, r1) = Party1FirstMessage::commit();
+    use super::*;
+
+    crate::test_for_all_curves!(test_coin_toss);
+    pub fn test_coin_toss<P>()
+    where P: ECPoint + Clone + Debug,
+          P::Scalar: PartialEq + Clone + Debug + Zeroize,
+    {
+        let (party1_first_message, m1, r1) = Party1FirstMessage::<PedersenProof<P>>::commit();
         let party2_first_message = Party2FirstMessage::share(&party1_first_message.proof);
         let (party1_second_message, random1) =
-            Party1SecondMessage::reveal(&party2_first_message.seed, &m1, &r1);
+            Party1SecondMessage::<P>::reveal(&party2_first_message.seed, &m1, &r1);
         let random2 = finalize(
             &party1_second_message.proof,
             &party2_first_message.seed,
--- src/cryptographic_primitives/twoparty/mod.rs
+++ src/cryptographic_primitives/twoparty/mod.rs
@@ -5,26 +5,26 @@
     License MIT: <https://github.com/KZen-networks/curv/blob/master/LICENSE>
 */
 
-/// This is an implementation of string coin tossing of a string, to generate a random string between
-///two non-trusting parties. Based on the
-/// the protocol and proof analysis  in "How To Simulate It – A Tutorial on the Simulation
-/// Proof Technique∗" (https://eprint.iacr.org/2016/046.pdf)
+// /// This is an implementation of string coin tossing of a string, to generate a random string between
+// ///two non-trusting parties. Based on the
+// /// the protocol and proof analysis  in "How To Simulate It – A Tutorial on the Simulation
+// r/// Proof Technique∗" (https://eprint.iacr.org/2016/046.pdf)
 pub mod coin_flip_optimal_rounds;
 
-///This is an implementation of a Diffie Hellman Key Exchange.
-/// Party1 private key is "x",
-/// Party2 private key is "y",
-/// The shared secret is Q = xyG
-pub mod dh_key_exchange;
+// ///This is an implementation of a Diffie Hellman Key Exchange.
+// /// Party1 private key is "x",
+// /// Party2 private key is "y",
+// /// The shared secret is Q = xyG
+// pub mod dh_key_exchange;
 
-///This is an implementation of a Diffie Hellman Key Exchange.
-/// Party1 private key is "x",
-/// Party2 private key is "y",
-///protocol:
-/// party1 sends a commitmemt to P1 = xG a commitment to a proof of knowledge of x
-/// party2 sends P2 and a proof of knowledge of y
-/// party1 verifies party2 proof decommit to P1 and  to the PoK
-/// party2 verifies party1 proof
-/// the shared secret is Q = xyG
-/// reference can be found in protocol 3.1 step 1 - 3(b) in the paper https://eprint.iacr.org/2017/552.pdf
-pub mod dh_key_exchange_variant_with_pok_comm;
+// ///This is an implementation of a Diffie Hellman Key Exchange.
+// /// Party1 private key is "x",
+// /// Party2 private key is "y",
+// ///protocol:
+// /// party1 sends a commitmemt to P1 = xG a commitment to a proof of knowledge of x
+// /// party2 sends P2 and a proof of knowledge of y
+// /// party1 verifies party2 proof decommit to P1 and  to the PoK
+// /// party2 verifies party1 proof
+// /// the shared secret is Q = xyG
+// /// reference can be found in protocol 3.1 step 1 - 3(b) in the paper https://eprint.iacr.org/2017/552.pdf
+// pub mod dh_key_exchange_variant_with_pok_comm;
