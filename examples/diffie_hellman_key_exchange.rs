94d113e5957617dc4dc749431615bc6aaeeee575
Denis Varlakov <dsurv@yandex.ru>
2020-11-01 14:20:25 +0700

    Update diffie_hellman_key_exchange example

--- examples/diffie_hellman_key_exchange.rs
+++ examples/diffie_hellman_key_exchange.rs
@@ -1,20 +1,28 @@
+#![cfg(feature = "ecc")]
+
+use std::fmt::Debug;
+
+use curv::elliptic::curves::traits::ECPoint;
+
 /// Diffie Hellman Key Exchange:
 /// TO RUN:
-/// cargo run --example diffie_hellman_key_exchange --features CURVE_NAME
+/// cargo run --example diffie_hellman_key_exchange --features ecc -- CURVE_NAME
 /// CURVE_NAME is any of the supported curves: i.e.:
-/// cargo run --example diffie_hellman_key_exchange --features ec_secp256k1
+/// cargo run --example diffie_hellman_key_exchange --features ecc -- secp256k1
 ///
 /// notice: this library includes also a more involved ECDH scheme. see
 /// dh_key_exchange_variant_with_pok_comm.rs
 
-#[cfg(feature = "ecc")]
-pub fn ecdh() {
+pub fn ecdh<P>()
+where P: ECPoint + Clone + Debug,
+      P::Scalar: Clone,
+{
     use curv::cryptographic_primitives::twoparty::dh_key_exchange::{
         compute_pubkey, Party1FirstMessage, Party2FirstMessage,
     };
 
-    let (kg_party_one_first_message, kg_ec_key_pair_party1) = Party1FirstMessage::first();
-    let (kg_party_two_first_message, kg_ec_key_pair_party2) = Party2FirstMessage::first();
+    let (kg_party_one_first_message, kg_ec_key_pair_party1) = Party1FirstMessage::<P>::first();
+    let (kg_party_two_first_message, kg_ec_key_pair_party2) = Party2FirstMessage::<P>::first();
 
     assert_eq!(
         compute_pubkey(
@@ -29,6 +37,15 @@ pub fn ecdh() {
 }
 
 fn main() {
-    #[cfg(feature = "ecc")]
-    ecdh();
+    let curve_name = std::env::args().nth(1);
+    match curve_name.as_ref().map(|s| s.as_str()) {
+        Some("secp256k1") => ecdh::<curv::elliptic::curves::secp256_k1::GE>(),
+        Some("ristretto") => ecdh::<curv::elliptic::curves::curve_ristretto::GE>(),
+        Some("ed25519")   => ecdh::<curv::elliptic::curves::ed25519::GE>(),
+        Some("jubjub")    => ecdh::<curv::elliptic::curves::curve_jubjub::GE>(),
+        Some("bls12_381") => ecdh::<curv::elliptic::curves::bls12_381::GE>(),
+        Some("p256")      => ecdh::<curv::elliptic::curves::p256::GE>(),
+        Some(unknown_curve) => eprintln!("Unknown curve: {}", unknown_curve),
+        None => eprintln!("Missing curve name"),
+    }
 }
