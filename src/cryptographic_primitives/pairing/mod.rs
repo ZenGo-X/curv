44cb7148dc887fdcf4e5cc8c698ebb804f4e18b8
doronz2 <doronz@gmail.com>
2020-11-09 15:17:25 +0200

    added: pairing

--- src/cryptographic_primitives/pairing/mod.rs
+++ src/cryptographic_primitives/pairing/mod.rs
@@ -1,15 +1,2 @@
 pub mod pairing_bls12_381;
 pub mod traits;
-mod bls;
-
-pub struct Signature{
-    public_key: GE,
-    private_key: FE,
-    signature: GE
-}
-
-impl Signature{
-    pub fn generate_key()-> FE{
-        FE::<ECScalar<>>::new_random()
-    }
-}
