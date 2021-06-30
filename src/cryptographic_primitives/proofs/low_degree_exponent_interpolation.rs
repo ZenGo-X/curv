b5f322e16562807ca6a9025b19c9303e877ab894
Denis <dsurv@yandex.ru>
2021-06-30 14:13:06 +0300

    LDEI prove takes witness and statement

--- src/cryptographic_primitives/proofs/low_degree_exponent_interpolation.rs
+++ src/cryptographic_primitives/proofs/low_degree_exponent_interpolation.rs
@@ -8,6 +8,14 @@ use crate::cryptographic_primitives::proofs::ProofError;
 use crate::cryptographic_primitives::secret_sharing::Polynomial;
 use crate::elliptic::curves::traits::ECPoint;
 
+/// The prover private polynomial
+#[derive(Derivative)]
+#[derivative(Clone(bound = "P::Scalar: Clone"))]
+#[derivative(Debug(bound = "P::Scalar: fmt::Debug"))]
+pub struct LdeiWitness<P: ECPoint> {
+    pub w: Polynomial<P>,
+}
+
 /// Claims that there's polynomial `w(x)` of degree `deg(w) <= degree`, and
 /// `forall i. x[i] = g[i] * alpha[i]` (and the prover knows `w(x)`)
 #[derive(Derivative)]
@@ -17,7 +25,42 @@ pub struct LdeiStatement<P: ECPoint> {
     pub alpha: Vec<P::Scalar>,
     pub g: Vec<P>,
     pub x: Vec<P>,
-    pub degree: u16,
+    pub d: u16,
+}
+
+impl<P> LdeiStatement<P>
+where
+    P: ECPoint + Clone,
+    P::Scalar: Clone + PartialEq,
+{
+    /// Takes [witness](LdeiWitness) (ie. secret polynomial `w(x)`), list of scalars `alpha`,
+    /// list of generators `g`, number `d`. Produces LdeiStatement consisting of `alpha`, `g`, `d`,
+    /// and list `x` such as `x_i = g_i * w(alpha_i)`
+    pub fn new(
+        witness: &LdeiWitness<P>,
+        alpha: Vec<P::Scalar>,
+        g: Vec<P>,
+        d: u16,
+    ) -> Result<Self, InvalidLdeiStatement> {
+        if g.len() != alpha.len() {
+            return Err(InvalidLdeiStatement::AlphaLengthDoesntMatchG);
+        }
+        if witness.w.degree() > d {
+            return Err(InvalidLdeiStatement::PolynomialDegreeMoreThanD);
+        }
+        if !ensure_list_is_pairwise_distinct(&alpha) {
+            return Err(InvalidLdeiStatement::AlphaNotPairwiseDistinct);
+        }
+        Ok(Self {
+            x: g.iter()
+                .zip(&alpha)
+                .map(|(g, a)| g.clone() * witness.w.evaluate(a))
+                .collect(),
+            alpha,
+            g,
+            d,
+        })
+    }
 }
 
 #[derive(Derivative)]
@@ -43,54 +86,46 @@ where
     /// `z(X) = u(X) − e · w(X)`. The proof is `(a_1,...,a_m,e,z)`.
     #[allow(clippy::many_single_char_names)]
     pub fn prove<H>(
-        w: &Polynomial<P>,
-        alpha: Vec<P::Scalar>,
-        g: Vec<P>,
-    ) -> Result<(LdeiStatement<P>, LdeiProof<P>), LdeiProofError>
+        witness: &LdeiWitness<P>,
+        statement: &LdeiStatement<P>,
+    ) -> Result<LdeiProof<P>, InvalidLdeiStatement>
     where
         H: Hash,
     {
-        if alpha.len() != g.len() {
-            return Err(LdeiProofError::AlphaLengthDoesntMatchG);
+        if statement.alpha.len() != statement.g.len() {
+            return Err(InvalidLdeiStatement::AlphaLengthDoesntMatchG);
+        }
+        if witness.w.degree() > statement.d {
+            return Err(InvalidLdeiStatement::PolynomialDegreeMoreThanD);
+        }
+        if !ensure_list_is_pairwise_distinct(&statement.alpha) {
+            return Err(InvalidLdeiStatement::AlphaNotPairwiseDistinct);
         }
 
-        // Check that alphas are pairwise distinct
-        for (i, a1) in alpha.iter().enumerate() {
-            for (j, a2) in alpha.iter().enumerate() {
-                if i != j && a1 == a2 {
-                    return Err(LdeiProofError::AlphaNotPairwiseDistinct);
-                }
-            }
-        }
-
-        let x: Vec<P> = g
+        let x_expected: Vec<P> = statement
+            .g
             .iter()
-            .zip(&alpha)
-            .map(|(g, a)| g.clone() * w.evaluate(a))
+            .zip(&statement.alpha)
+            .map(|(g, a)| g.clone() * witness.w.evaluate(a))
             .collect();
+        if statement.x != x_expected {
+            return Err(InvalidLdeiStatement::ListOfXDoesntMatchExpectedValue);
+        }
 
-        let d = w.degree();
-        let u = Polynomial::<P>::sample(d);
-        let a: Vec<P> = g
+        let u = Polynomial::<P>::sample(statement.d);
+        let a: Vec<P> = statement
+            .g
             .iter()
-            .zip(&alpha)
+            .zip(&statement.alpha)
             .map(|(g, a)| g.clone() * u.evaluate(a))
             .collect();
 
-        let hash_input: Vec<&P> = g.iter().chain(&x).chain(&a).collect();
+        let hash_input: Vec<&P> = statement.g.iter().chain(&statement.x).chain(&a).collect();
         let e = H::create_hash_from_ge::<P>(hash_input.as_slice());
 
-        let z = &u - &(w * &e);
+        let z = &u - &(&witness.w * &e);
 
-        let statement = LdeiStatement {
-            alpha,
-            g,
-            x,
-            degree: d,
-        };
-        let proof = LdeiProof { a, e, z };
-
-        Ok((statement, proof))
+        Ok(LdeiProof { a, e, z })
     }
 
     /// Verifies correctness of a statement
@@ -114,7 +149,7 @@ where
         if e != self.e {
             return Err(ProofError);
         }
-        if self.z.degree() > statement.degree {
+        if self.z.degree() > statement.d {
             return Err(ProofError);
         }
 
@@ -134,12 +169,28 @@ where
     }
 }
 
+/// Indicates that statement is not valid or doesn't match a witness
 #[derive(Debug, Error)]
-pub enum LdeiProofError {
+pub enum InvalidLdeiStatement {
     #[error("`alpha`s are not pairwise distinct")]
     AlphaNotPairwiseDistinct,
     #[error("alpha.len() != g.len()")]
     AlphaLengthDoesntMatchG,
+    #[error("deg(w) > d")]
+    PolynomialDegreeMoreThanD,
+    #[error("`statement.x` doesn't match expected value")]
+    ListOfXDoesntMatchExpectedValue,
+}
+
+fn ensure_list_is_pairwise_distinct<S: PartialEq>(list: &[S]) -> bool {
+    for (i, x1) in list.iter().enumerate() {
+        for (j, x2) in list.iter().enumerate() {
+            if i != j && x1 == x2 {
+                return false;
+            }
+        }
+    }
+    true
 }
 
 #[cfg(test)]
@@ -159,7 +210,9 @@ mod tests {
         P: ECPoint + Clone + PartialEq,
         P::Scalar: ECScalar + Clone + PartialEq,
     {
+        let d = 5;
         let poly = Polynomial::<P>::sample_exact(5);
+        let witness = LdeiWitness { w: poly };
 
         let alpha: Vec<P::Scalar> = (1..=10).map(|i| ECScalar::from(&BigInt::from(i))).collect();
         let g: Vec<P> = iter::repeat_with(ECScalar::new_random)
@@ -167,49 +220,11 @@ mod tests {
             .take(10)
             .collect();
 
-        let (statement, proof) =
-            LdeiProof::prove::<HSha256>(&poly, alpha, g).expect("failed to prove");
+        let statement = LdeiStatement::new(&witness, alpha, g, d).unwrap();
+
+        let proof = LdeiProof::prove::<HSha256>(&witness, &statement).expect("failed to prove");
         proof
             .verify::<HSha256>(&statement)
             .expect("failed to validate proof");
-    }
-
-    test_for_all_curves!(catches_invalid_args);
-    fn catches_invalid_args<P>()
-    where
-        P: ECPoint + Clone + PartialEq + fmt::Debug,
-        P::Scalar: ECScalar + Clone + PartialEq + fmt::Debug,
-    {
-        let poly = Polynomial::<P>::sample_exact(5);
-
-        // Test that prove requires alphas.len() == g.len()
-        {
-            let alpha: Vec<P::Scalar> =
-                (2..=10).map(|i| ECScalar::from(&BigInt::from(i))).collect();
-            let g: Vec<P> = iter::repeat_with(ECScalar::new_random)
-                .map(|x| P::generator() * x)
-                .take(10)
-                .collect();
-            let result = LdeiProof::prove::<HSha256>(&poly, alpha, g);
-            if !matches!(result, Err(LdeiProofError::AlphaLengthDoesntMatchG)) {
-                panic!("Unexpected result: {:?}", result);
-            }
-        }
-
-        // Test that prove requires alphas to be pairwise distinct
-        {
-            let alpha: Vec<P::Scalar> = iter::once(2)
-                .chain(2..=10)
-                .map(|i| ECScalar::from(&BigInt::from(i)))
-                .collect();
-            let g: Vec<P> = iter::repeat_with(ECScalar::new_random)
-                .map(|x| P::generator() * x)
-                .take(10)
-                .collect();
-            let result = LdeiProof::prove::<HSha256>(&poly, alpha, g);
-            if !matches!(result, Err(LdeiProofError::AlphaNotPairwiseDistinct)) {
-                panic!("Unexpected result: {:?}", result);
-            }
-        }
     }
 }
