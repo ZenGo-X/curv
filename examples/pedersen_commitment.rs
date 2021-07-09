c90c3fc19f718a92e2b4f6791968e13013aa4d43
Denis <dsurv@yandex.ru>
2021-07-09 16:00:22 +0300

    Update pedersen_commitment

--- examples/pedersen_commitment.rs
+++ examples/pedersen_commitment.rs
@@ -1,5 +1,5 @@
-use curv::arithmetic::{traits::*, BigInt};
-use curv::elliptic::curves::traits::ECPoint;
+use curv::arithmetic::*;
+use curv::elliptic::curves::*;
 
 use std::fmt::Debug;
 
@@ -14,16 +14,13 @@ use std::fmt::Debug;
 ///
 /// notice: this library includes also hash based commitments
 
-pub fn ped_com<P>(message: &BigInt)
-where
-    P: ECPoint + Debug,
-{
+pub fn ped_com<E: Curve>(message: &BigInt) {
     use curv::cryptographic_primitives::commitments::pedersen_commitment::PedersenCommitment;
     use curv::cryptographic_primitives::commitments::traits::Commitment;
 
     let security_bits = 256;
     let blinding_factor = BigInt::sample(security_bits);
-    let com = PedersenCommitment::<P>::create_commitment_with_user_defined_randomness(
+    let com = PedersenCommitment::<E>::create_commitment_with_user_defined_randomness(
         message,
         &blinding_factor,
     );
@@ -36,15 +33,14 @@ where
 
 fn main() {
     let message = "commit me!";
-    let message_bytes = message.as_bytes();
-    let _message_bn = BigInt::from_bytes(message_bytes);
+    let _message_bn = BigInt::from_bytes(message.as_bytes());
     let curve_name = std::env::args().nth(1);
     match curve_name.as_deref() {
-        Some("secp256k1") => ped_com::<curv::elliptic::curves::secp256_k1::GE>(&_message_bn),
-        Some("ristretto") => ped_com::<curv::elliptic::curves::curve_ristretto::GE>(&_message_bn),
-        Some("ed25519") => ped_com::<curv::elliptic::curves::ed25519::GE>(&_message_bn),
-        Some("bls12_381") => ped_com::<curv::elliptic::curves::bls12_381::g1::GE>(&_message_bn),
-        Some("p256") => ped_com::<curv::elliptic::curves::p256::GE>(&_message_bn),
+        Some("secp256k1") => ped_com::<Secp256k1>(&_message_bn),
+        // Some("ristretto") => ped_com::<curv::elliptic::curves::curve_ristretto::GE>(&_message_bn),
+        // Some("ed25519") => ped_com::<curv::elliptic::curves::ed25519::GE>(&_message_bn),
+        // Some("bls12_381") => ped_com::<curv::elliptic::curves::bls12_381::g1::GE>(&_message_bn),
+        // Some("p256") => ped_com::<curv::elliptic::curves::p256::GE>(&_message_bn),
         Some(unknown_curve) => eprintln!("Unknown curve: {}", unknown_curve),
         None => eprintln!("Missing curve name"),
     }
