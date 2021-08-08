5225da6439f05748fec6ccc70ff6078ff6bce55e
Elichai Turkel <elichai.turkel@gmail.com>
2021-08-08 19:50:32 +0300

    Add a macro to test against a bunch of hash functions and move test macros to a new module

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,8 @@ pub use crate::arithmetic::BigInt;
 
 pub mod cryptographic_primitives;
 
+mod test_utils;
+
 #[derive(Copy, PartialEq, Eq, Clone, Debug)]
 pub enum ErrorKey {
     InvalidPublicKey,
@@ -18,49 +20,4 @@ pub enum ErrorKey {
 
 pub enum ErrorSS {
     VerifyShareError,
-}
-
-#[cfg(test)]
-#[macro_export]
-macro_rules! test_for_all_curves {
-    (#[should_panic] $fn: ident) => {
-        crate::test_for_all_curves!([#[should_panic]] $fn);
-    };
-    ($fn: ident) => {
-        crate::test_for_all_curves!([] $fn);
-    };
-    ([$($attrs:tt)*] $fn: ident) => {
-        paste::paste!{
-            #[test]
-            $($attrs)*
-            fn [<$fn _secp256k1>]() {
-                $fn::<crate::elliptic::curves::Secp256k1>()
-            }
-            #[test]
-            $($attrs)*
-            fn [<$fn _p256>]() {
-                $fn::<crate::elliptic::curves::Secp256r1>()
-            }
-            #[test]
-            $($attrs)*
-            fn [<$fn _ed25519>]() {
-                $fn::<crate::elliptic::curves::Ed25519>()
-            }
-            #[test]
-            $($attrs)*
-            fn [<$fn _ristretto>]() {
-                $fn::<crate::elliptic::curves::Ristretto>()
-            }
-            #[test]
-            $($attrs)*
-            fn [<$fn _bls12_381_1>]() {
-                $fn::<crate::elliptic::curves::Bls12_381_1>()
-            }
-            #[test]
-            $($attrs)*
-            fn [<$fn _bls12_381_2>]() {
-                $fn::<crate::elliptic::curves::Bls12_381_2>()
-            }
-        }
-    };
 }
