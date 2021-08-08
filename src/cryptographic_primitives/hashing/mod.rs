fd80d79464f3421543ebf7c0eaf364d8312830f9
Elichai Turkel <elichai.turkel@gmail.com>
2021-08-08 19:42:42 +0300

    Delete deprecated hashing traits and structures

--- src/cryptographic_primitives/hashing/mod.rs
+++ src/cryptographic_primitives/hashing/mod.rs
@@ -4,13 +4,8 @@
     (https://github.com/KZen-networks/curv)
     License MIT: https://github.com/KZen-networks/curv/blob/master/LICENSE
 */
-pub mod blake2b512;
-pub mod hash_sha256;
-pub mod hash_sha512;
-pub mod hmac_sha512;
-pub mod merkle_tree;
-pub mod traits;
-
 mod ext;
+pub mod merkle_tree;
+
 pub use digest::Digest;
 pub use ext::*;
