6bfa167f9b0ffb6bb98c65640533b668d3d93be5
Elichai Turkel <elichai.turkel@gmail.com>
2023-03-29 14:55:25 +0300

    Implement std::error::Error for the error types (#117)
    Co-authored-by: Iraklis Leontiadis <120665504+leontiadZen@users.noreply.github.com>

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,7 @@ pub mod elliptic;
 
 pub mod arithmetic;
 pub use crate::arithmetic::BigInt;
+use std::{error, fmt};
 
 pub mod cryptographic_primitives;
 
@@ -21,6 +22,65 @@ pub enum ErrorKey {
     InvalidPublicKey,
 }
 
+impl fmt::Display for ErrorKey {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ErrorKey::InvalidPublicKey => f.write_str("Invalid Public Key"),
+        }
+    }
+}
+impl error::Error for ErrorKey {}
+
+#[derive(Debug)]
 pub enum ErrorSS {
     VerifyShareError,
+}
+
+impl fmt::Display for ErrorSS {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            ErrorSS::VerifyShareError => f.write_str("Failed verifying the Secret Share"),
+        }
+    }
+}
+impl error::Error for ErrorSS {}
+
+#[cfg(test)]
+#[macro_export]
+macro_rules! test_for_all_curves {
+    (#[should_panic] $fn: ident) => {
+        crate::test_for_all_curves!([#[should_panic]] $fn);
+    };
+    ($fn: ident) => {
+        crate::test_for_all_curves!([] $fn);
+    };
+    ([$($attrs:tt)*] $fn: ident) => {
+        paste::paste!{
+            #[test]
+            $($attrs)*
+            fn [<$fn _secp256k1>]() {
+                $fn::<crate::elliptic::curves::secp256_k1::GE>()
+            }
+            #[test]
+            $($attrs)*
+            fn [<$fn _ristretto>]() {
+                $fn::<crate::elliptic::curves::curve_ristretto::GE>()
+            }
+            #[test]
+            $($attrs)*
+            fn [<$fn _ed25519>]() {
+                $fn::<crate::elliptic::curves::ed25519::GE>()
+            }
+            #[test]
+            $($attrs)*
+            fn [<$fn _bls12_381>]() {
+                $fn::<crate::elliptic::curves::bls12_381::g1::GE>()
+            }
+            #[test]
+            $($attrs)*
+            fn [<$fn _p256>]() {
+                $fn::<crate::elliptic::curves::p256::GE>()
+            }
+        }
+    };
 }
